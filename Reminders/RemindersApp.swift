6ca169db22edce4411c94765a60f6491167a15a0
sangam <sangampokharel803@gmail.com>
2025-09-03 10:06:54 +0545

    Initial Commit

---
+++ Reminders/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Reminders
+//
+//  Created by sangam pokharel on 03/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
